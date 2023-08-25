8b426d4d10a464c8418028043680b631dcf5bb1a
grif0241 <grif0241@algonquinlive.com>
2023-08-25 13:00:52 -0400

    Restore file structure

---
+++ output.ts
@@ -0,0 +1,83 @@
+
+      "// ==========================================\n*** GENERATED FILE. DO NOT EDIT ***\nAll local changes will be overwritten.\nLast updated: 1692982838063\n// ==========================================\n"
+
+      export interface Phrase {
+        en: string;
+        fr: string;
+        es: string;
+      }
+
+      export const Phrases = new Map<string, Phrase>([
+  [
+    "general.cancel",
+    {
+      "en": "Cancel",
+      "fr": "Annuler",
+      "es": "Cancelar"
+    }
+  ],
+  [
+    "general.delete",
+    {
+      "en": "Delete",
+      "fr": "Supprimer",
+      "es": "Suprimir"
+    }
+  ],
+  [
+    "general.view",
+    {
+      "en": "View",
+      "fr": "Afficher",
+      "es": "Ver"
+    }
+  ],
+  [
+    "general.back",
+    {
+      "en": "Back",
+      "fr": "Retour",
+      "es": "Anterior"
+    }
+  ],
+  [
+    "general.close",
+    {
+      "en": "Close",
+      "fr": "Fermer",
+      "es": "Cerrar"
+    }
+  ],
+  [
+    "general.error",
+    {
+      "en": "Error",
+      "fr": "Erreur",
+      "es": "Error"
+    }
+  ],
+  [
+    "general.externalLink",
+    {
+      "en": "external link",
+      "fr": "Lien externe",
+      "es": ""
+    }
+  ],
+  [
+    "general.externalLinkTitle",
+    {
+      "en": "Opens in a new window",
+      "fr": "Ouvre dans une nouvelles fenatre",
+      "es": ""
+    }
+  ],
+  [
+    "general.next",
+    {
+      "en": "Next",
+      "fr": "Suivant",
+      "es": "Siguiente"
+    }
+  ]
+])
